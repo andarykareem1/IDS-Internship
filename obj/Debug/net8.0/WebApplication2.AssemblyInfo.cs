d586031976acd7d63a5bc8879f0f9d117edc0489
andarykareem1 <157199171+andarykareem1@users.noreply.github.com>
2025-12-16 15:48:14 +0200

    Remove build artifacts and keep source code only

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412bc9949637e06614515dad56fbf759312eff46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c28b3c97ffe11bda9f5d79927b0debb5f7aba97")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
